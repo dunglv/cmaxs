3b0b6ec8eaf457896518972169337e982f28f318
Quang <quangpm@rikkeisoft.com>
2018-05-04 12:48:13 +0700

    Update configure docker

--- conf.d/startup.sh
+++ conf.d/startup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 # startup.sh
-/usr/bin/supervisord
-supervisord -c /etc/supervisor/supervisord.conf
-supervisorctl -c /etc/supervisor/supervisord.conf
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
